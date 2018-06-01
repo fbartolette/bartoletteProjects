1286f9e2910afc24edcc6bd72b66c662fdffff12
FBARTOL <FBARTOL@RJO-BCC-C2A-H19.brasilcenter.com.br>
2018-06-01 15:29:42 -0300

    primeiro commit

---
+++ FbartoletteProjects/src/tests/simpleTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class simpleTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		//TODO
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		//TODO
+	}
+
+	@Test
+	void test() {
+		//TODO
+	}
+
+}
